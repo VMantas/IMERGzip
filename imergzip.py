bf2e90a4d2660b2d1fdb73018597833c2af22c8e
VMantas <vasco.mantas@gmail.com>
2024-09-16 17:10:14 +0100

    Update imergzip.py

--- imergzip.py
+++ imergzip.py
@@ -29,12 +29,12 @@ def get_precipitation_for_zip(df, zip_code):
 df = load_data()
 
 # Streamlit app UI
-st.title("IMERG Precipitation Climatology")
+st.title("IMERG Precipitation Climatology (Prototype)")
 
 # Add a description for IMERG
 st.markdown("""
 ### What is IMERG?
-The Integrated Multi-satellite Retrievals for GPM (IMERG) is a product of the Global Precipitation Measurement (GPM) mission. It combines satellite observations from a constellation of satellites to provide global estimates of precipitation. This allows scientists and researchers to track precipitation trends, identify extreme weather events, and monitor water resources across the globe in near real-time.
+The Integrated Multi-satellite Retrievals for GPM (IMERG) is a product of the Global Precipitation Measurement (GPM) mission. It combines satellite observations from a constellation of satellites to provide global estimates of precipitation. This allows scientists and researchers to track precipitation trends, identify extreme weather events, and monitor water resources across the globe in near real-time. Use the data in this website for educational purposes only. NO quality control was done. This is NOT an official wesite or affiliated with NASA in any way.
 
 More information about IMERG can be found on the [NASA IMERG website](https://gpm.nasa.gov/data/imerg).
 """)
